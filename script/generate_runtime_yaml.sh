6c74751150b2d44e5f8ad8831bd92a97588b5874
cbluebird <crk2778355976@163.com>
2024-09-13 15:54:07 +0800

    chore: Update build and push scripts

--- script/generate_runtime_yaml.sh
+++ script/generate_runtime_yaml.sh
@@ -9,12 +9,21 @@ echo "TAG=$TAG"
 IFS=',' read -r -a DIFF_OUTPUT_ARRAY <<< "$DIFF_OUTPUT"
 IFS=',' read -r -a PARENT_DIRS_ARRAY <<< "$PARENT_DIRS"
 
+declare -A NAME_MAP
+while IFS='=' read -r key value; do
+    NAME_MAP["$key"]="$value"
+done < "configs/name.txt" 
+
 for i in "${!DIFF_OUTPUT_ARRAY[@]}"; do
   DOCKERFILE_PATH=${DIFF_OUTPUT_ARRAY[$i]}
   IFS='/' read -ra ADDR <<< $DOCKERFILE_PATH
+
   PARENT_DIR=${PARENT_DIRS_ARRAY[$i]}
   IMAGE_NAME="${ADDR[1]}-$PARENT_DIR:$TAG"
-  IMAGE_NAME="${IMAGE_NAME// /-}"
+  
+  if [ "${NAME_MAP[${ADDR[1]}]}" == "none" ]; then
+    exit 1
+  fi
 
   YAML_PATH="${DOCKERFILE_PATH%/*}"
   mkdir -p "yaml/${YAML_PATH}"
@@ -37,6 +46,12 @@ spec:
         name: devbox-ssh-port
         protocol: TCP
     user: sealos
+    workingDir: /home/sealos/project
+    releaseCommand:
+      - /bin/bash
+      - -c
+    releaseArgs:
+      - /home/sealos/project/entrypoint.sh
   description: ${ADDR[1]} $PARENT_DIR
   version: "$PARENT_DIR"
 ---
@@ -45,7 +60,7 @@ kind: RuntimeClass
 metadata:
   name: ${ADDR[1]}
 spec:
-  title: ${ADDR[1]}
+  title: "${NAME_MAP[${ADDR[1]}]}"
   kind: ${ADDR[0]}
   description: ${ADDR[1]}
 EOF
