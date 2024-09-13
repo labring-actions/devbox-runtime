81b7ff32c9568eb27ace5735c835797830961380
cbluebird <crk2778355976@163.com>
2024-09-13 20:27:31 +0800

    chore: Update Dockerfile to use latest version of debian-ssh-12.6 image

--- Language/go/project/main.go
+++ Language/go/project/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
-  "fmt"
+    "fmt"
+    "net/http"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, World!")
+}
+
 func main() {
-  fmt.Println("Hello World!")
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":8080", nil)
 }
