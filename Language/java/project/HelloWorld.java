a264e6e213c119af156c8d2e2168cf319f1b65da
cbluebird <crk2778355976@163.com>
2024-09-13 20:32:56 +0800

    feat: Update Dockerfile to use latest version of debian-ssh-12.6 image and add dumb-init for process management

--- Language/java/project/HelloWorld.java
+++ Language/java/project/HelloWorld.java
@@ -1,7 +1,26 @@
-// Your First Program
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
 
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class HelloWorld {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null);
+        server.start();
+    }
+
+    static class MyHandler implements HttpHandler {
+        public void handle(HttpExchange exchange) throws IOException {
+            String response = "Hello, World!";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
     }
 }
