b66237f5a39fe47dc0c289afe615dee45155d24c
cbluebird <crk2778355976@163.com>
2024-10-29 11:49:39 +0800

    bugfix: fix the vue server and port

--- Framework/vue/v3.4.29/project/vite.config.js
+++ Framework/vue/v3.4.29/project/vite.config.js
@@ -8,6 +8,10 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  server: {
+    host: "0.0.0.0",
+    port: "3000"
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
