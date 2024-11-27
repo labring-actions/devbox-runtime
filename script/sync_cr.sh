5374a9a1de09b9fc0d8a712ffe699020af0df302
cbluebird <crk2778355976@163.com>
2024-11-27 12:13:15 +0800

    Refactor sync_cr.sh script to fix error in reading runtime information

--- script/sync_cr.sh
+++ script/sync_cr.sh
@@ -7,7 +7,7 @@ kubectl get runtime -n devbox-system > "$TMP_FILE"
 mapfile -t runtime_lines < "$TMP_FILE"
 
 for line in "${runtime_lines[@]:1}"; do
-  read -r namespace name class version runtime_version state <<< "$line"
+  read -r  name class version runtime_version state <<< "$line"
 
     if [[ -z "$class" ]]; then
       continue
@@ -20,7 +20,7 @@ for line in "${runtime_lines[@]:1}"; do
 
     if [ "$state" = "active" ];then
       read class version image < <(kubectl get runtime $name -n devbox-system -o json | jq -r 'select(.spec.state == "active") | [.spec.classRef, .spec.version, .spec.config.image] | @tsv')
-      read kind < <(kubectl get runtimeclass.devbox "$class" -n devbox-system -o json | jq -r '.spec.kind')
+      read kind < <(kubectl get runtimeclass.devbox $class -n devbox-system -o json | jq -r '.spec.kind')
       ./runtimectl gen --kind=$kind --name=$class --version=$version --image=$image --path=$1
     fi
 done
