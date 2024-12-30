0afe6b2521a033b8ee03d04f41164e61892b22d3
cbluebird <crk2778355976@163.com>
2024-12-30 17:40:11 +0800

    Refactor Dockerfile to include conda activation and update entrypoint.sh

--- Framework/pytorch/project/hello.py
+++ Framework/pytorch/project/hello.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn.functional as F
 
-# 自定义CUDA内核
 def fibonacci_cuda_kernel(n, fib):
     if n < 2:
         return
@@ -11,18 +10,14 @@ def fibonacci_cuda_kernel(n, fib):
         fib[i] = fib[i - 1] + fib[i - 2]
 
 def fibonacci_cuda(n):
-    # 创建一个CUDA张量来存储斐波那契数列
     fib = torch.zeros(n, device='cuda')
 
-    # 调用自定义CUDA内核
     fibonacci_cuda_kernel(n, fib)
 
     return fib
 
-# 获取cuda版本
 print(torch.__version__)
 print(torch.cuda.is_available())
-# 示例：计算前100个斐波那契数
 n = 10
 result = fibonacci_cuda(n)
-print(result[9].cpu()) # 将结果从CUDA设备转移到CPU并打印
+print(result[9].cpu())
