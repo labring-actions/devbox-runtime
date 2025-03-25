14ff0c7935c34193acfb19a4b06092bea3f9d7ba
yy <lingdie.yy@outlook.com>
2025-03-25 16:10:17 +0800

    fix: ci syntax error

--- Language/python/update_cn_dockerfile.sh
+++ Language/python/update_cn_dockerfile.sh
@@ -1,18 +1,39 @@
 #!/bin/bash
 
 DOCKERFILE=$1
+OS_TYPE=${2:-"linux"}
 echo "DOCKERFILE: $DOCKERFILE"
 TMP_DOCKERFILE="${DOCKERFILE}tmp"
 cp "$DOCKERFILE" "$TMP_DOCKERFILE"
 
-sed -i '$i\
+# Different sed syntax for macOS vs Linux
+if [[ "$OS_TYPE" == "darwin" ]]; then
+  # macOS version
+  sed -i '' '$i\
 COPY /OS/debian-ssh/debian.sources /etc/apt/sources.list.d/debian.sources' "$TMP_DOCKERFILE"
 
-sed -i '$i\
+  sed -i '' '$i\
 RUN pip config set global.index-url https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple' "$TMP_DOCKERFILE"
 
-sed -i '$i\
+  sed -i '' '$i\
 USER devbox' "$TMP_DOCKERFILE"
 
-sed -i '$i\
+  sed -i '' '$i\
 RUN pip config set global.index-url https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple' "$TMP_DOCKERFILE"
+else
+  # Linux version
+  sed -i '$i\
+COPY /OS/debian-ssh/debian.sources /etc/apt/sources.list.d/debian.sources' "$TMP_DOCKERFILE"
+
+  sed -i '$i\
+RUN pip config set global.index-url https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple' "$TMP_DOCKERFILE"
+
+  sed -i '$i\
+USER devbox' "$TMP_DOCKERFILE"
+
+  sed -i '$i\
+RUN pip config set global.index-url https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple' "$TMP_DOCKERFILE"
+fi
+
+# Replace original file with modified one
+mv "$TMP_DOCKERFILE" "$DOCKERFILE"
--- script/build_and_push_images.sh
+++ script/build_and_push_images.sh
@@ -14,10 +14,18 @@ function build_and_push_image() {
 
 function execute_cn_patch() {
   script_dir=$(dirname "$(dirname "$build_target")")
-  bash "$script_dir/update_cn_dockerfile.sh" "$build_target"
+  # Use a compatible approach for both Linux and macOS
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS requires an argument for -i
+    bash "$script_dir/update_cn_dockerfile.sh" "$build_target" "darwin"
+  else
+    # Linux version
+    bash "$script_dir/update_cn_dockerfile.sh" "$build_target" "linux"
+  fi
 }
 
 if [ "$is_cn" -eq 1 ]; then
+  echo "executing cn patch"
   execute_cn_patch
 fi
 
