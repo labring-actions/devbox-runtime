4dd802a1958ee1c0b37d54713fd6386486a090df
抒情熊 <2669184984@qq.com>
2025-04-01 16:09:51 +0800

    add springboot.

---
+++ Service/mcp/spring-boot-3-3-2/project/src/main/java/com/example/service/DemoService.java
@@ -0,0 +1,20 @@
+package com.example.service;
+
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoService {
+    /**
+     * Demo tool call.
+     */
+    @Tool(description = "demo tool call")
+    public String toolCall() {
+        return "test";
+    }
+
+    public static void main(String[] args) {
+        DemoService client = new DemoService();
+    }
+
+}
