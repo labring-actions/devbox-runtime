c22d3fb4a82db66c4f90734fc6ecc947f6c93b36
mlhiter <3076438032@qq.com>
2025-04-01 17:31:40 +0800

    chore: entrypoint.sh

--- Service/mcp/csharp-1-0/project/entrypoint.sh
+++ Service/mcp/csharp-1-0/project/entrypoint.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
-node hello.js
+app_env=${1:-development}
+
+# Development environment commands
+dev_commands() {
+    echo "Running Svelte development environment..."
+    dotnet run
+}
+
+# Production environment commands
+prod_commands() {
+    echo "Running Svelte production build..."
+    dotnet publish
+    echo "Starting Svelte production preview server..."
+    dotnet run -- --host 0.0.0.0 --port 3001
+}
+
+# Check environment variables to determine the running environment
+if [ "$app_env" = "production" ] || [ "$app_env" = "prod" ] ; then
+    echo "Production environment detected"
+    prod_commands
+else
+    echo "Development environment detected"
+    dev_commands
+fi
--- Service/mcp/csharp-1-0/project/test.sh
+++
@@ -1,8 +0,0 @@
-sudo wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh && \
-sudo chmod -R +x ./dotnet-install.sh && \
-sudo ./dotnet-install.sh --version 9.0.100 -InstallDir /usr/share/dotnet/ && \
-echo 'export PATH=$PATH:/usr/share/dotnet/' >> ~/.bashrc && \
-source ~/.bashrc && \
-sudo apt-get update && sudo apt-get install -y libicu-dev && \
-dotnet add package ModelContextProtocol --prerelease && \
-dotnet add package ModelContextProtocol.AspNetCore --prerelease && \
