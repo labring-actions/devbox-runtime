02b159a39b79e28ca26787176f956526d8f7d499
Bearix <2669184984@qq.com>
2025-04-02 11:54:42 +0800

    feat: add mcp service, python and java sse server. (#20)
    * add python mcp sse server.
    
    * fix python image bug.
    
    * add springboot.
    
    * add springboot.
    
    * fix python bug.
    
    * add readme.
    
    * fix python readme bug.
    
    * change readme location.
    
    * change python image.
    
    * change python image.
    
    * change java image.
    
    * change java image.

---
+++ Service/mcp/spring-boot-3-3-2/project/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+app_env=${1:-development}
+
+# Development environment commands
+dev_commands() {
+    echo "Running development environment commands..."
+    mvn spring-boot:run
+}
+
+# Production environment commands
+prod_commands() {
+    echo "Running production environment commands..."
+    mvn clean install
+    mvn spring-boot:run
+}
+
+# prod_commands
+# Check environment variables to determine the running environment
+if [ "$app_env" = "production" ] || [ "$app_env" = "prod" ] ; then
+    echo "Production environment detected"
+    prod_commands
+else
+    echo "Development environment detected"
+    dev_commands
+fi
