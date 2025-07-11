d3a0ba4107749ad70faac7c020a1049aad52aa15
Cunzili <cunzili@CunzilideMacBook-Pro.local>
2025-07-11 14:24:54 +0800

    不同镜像放到不同的ACR repo里

--- script/get_image_name.sh
+++ script/get_image_name.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-if [ "$#" -ne 5 ]; then
-  echo "Usage: $0 <registry> <namespace> <repo> <build_target> <tag>"
+if [ "$#" -ne 4 ]; then
+  echo "Usage: $0 <registry> <namespace> <build_target> <tag>"
   exit 1
 fi
 
 REGISTRY=$1
 NAMESPACE=$2
-REPO=$3
-BUILD_TARGET=$4
-TAG=$5
+BUILD_TARGET=$3
+TAG=$4
+
+# 提取镜像标识
+IFS='/' read -ra ADDR <<< "$BUILD_TARGET"
+IMAGE_NAME="${ADDR[1]}-${ADDR[2]}"
 
 if [[ "$REGISTRY" == ghcr.io* ]]; then
   # ghcr.io 格式
-  IFS='/' read -ra ADDR <<< "$BUILD_TARGET"
-  IMAGE_NAME="${ADDR[1]}-${ADDR[2]}"
   echo "$REGISTRY/$NAMESPACE/devbox/$IMAGE_NAME:$TAG"
 else
-  # ACR 格式，tag 自动拼接镜像标识
-  IFS='/' read -ra ADDR <<< "$BUILD_TARGET"
-  IMAGE_ID="${ADDR[1]}-${ADDR[2]}"
-  echo "$REGISTRY/$NAMESPACE/$REPO:${IMAGE_ID}-$TAG"
+  # ACR 格式
+  echo "$REGISTRY/$NAMESPACE/$IMAGE_NAME:$TAG"
 fi
