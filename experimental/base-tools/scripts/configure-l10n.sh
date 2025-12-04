c36940ba1738d0da3d9ce527462b4c3f2f09727d
Yun Pan <dinoallo@netc.it>
2025-12-04 16:14:08 +0800

    fix: correct path to locale configuration script in configure-l10n.sh
    Signed-off-by: Yun Pan <dinoallo@netc.it>

--- experimental/base-tools/scripts/configure-l10n.sh
+++ experimental/base-tools/scripts/configure-l10n.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 L10N=${L10N:-en_US}
 SOURCE_DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 echo "Current L10N is set to: $L10N"
-if [ -f "$SOURCE_DIR/$L10N/configure.sh" ]; then
+if [ -f "$SOURCE_DIR/l10n/$L10N/configure.sh" ]; then
   echo "Configuring locale for L10N=$L10N"
   chmod +x "$SOURCE_DIR/$L10N/"*.sh 
   "$SOURCE_DIR/$L10N/configure.sh"
