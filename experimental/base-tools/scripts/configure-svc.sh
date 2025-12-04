ebefa7f3b224cb3605deefd5a1db703eccf1e263
Yun Pan <dinoallo@netc.it>
2025-12-04 16:04:40 +0800

    refactor: update service configuration order and improve pre-rc-init script documentation
    Signed-off-by: Yun Pan <dinoallo@netc.it>

--- experimental/base-tools/scripts/configure-svc.sh
+++ experimental/base-tools/scripts/configure-svc.sh
@@ -5,10 +5,8 @@ chmod +x $SOURCE_DIR/svc/*.sh
 # Important: s6 must be configured before other services that depend on it
 $SOURCE_DIR/svc/configure-s6.sh
 
-# Configure pre-rc-init hook FIRST
-# This hook runs BEFORE s6-rc compilation, allowing us to disable services
-# based on DEVBOX_ENV environment variable
-$SOURCE_DIR/svc/configure-pre-rc-init.sh
+# Configure hook FIRST
+$SOURCE_DIR/svc/configure-s6-hook.sh
 
 # Configure individual services
 $SOURCE_DIR/svc/configure-startup.sh
