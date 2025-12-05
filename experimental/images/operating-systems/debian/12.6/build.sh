d489b3e35d3a7b3984790dd87f9e57c54c38ead5
Yun Pan <dinoallo@netc.it>
2025-12-05 11:52:09 +0800

    fix: improve quoting for variable expansions in scripts for consistency and reliability
    Signed-off-by: Yun Pan <dinoallo@netc.it>

--- experimental/images/operating-systems/debian/12.6/build.sh
+++ experimental/images/operating-systems/debian/12.6/build.sh
@@ -6,12 +6,12 @@ echo "Current L10N: $L10N"
 echo "Current ARCH: $ARCH"
 echo "Current DEFAULT_DEVBOX_USER: $DEFAULT_DEVBOX_USER"
 
-chmod +x $BASE_TOOLS_DIR/scripts/*.sh
+chmod +x "$BASE_TOOLS_DIR/scripts/"*.sh
 
 # Install base packages for Debian
 $BASE_TOOLS_DIR/scripts/install-base-pkg-deb.sh
 
-# Instal cron and s6 from base-tools scripts
+# Install cron and s6 from base-tools scripts
 $BASE_TOOLS_DIR/scripts/install-crond.sh
 $BASE_TOOLS_DIR/scripts/install-s6.sh
 $BASE_TOOLS_DIR/scripts/install-sdk-server.sh
@@ -28,7 +28,7 @@ $BASE_TOOLS_DIR/scripts/configure-login.sh
 $BASE_TOOLS_DIR/scripts/configure-l10n.sh
 
 # Configure user devbox
-$BASE_TOOLS_DIR/scripts/configure-user.sh $DEFAULT_DEVBOX_USER
+$BASE_TOOLS_DIR/scripts/configure-user.sh "$DEFAULT_DEVBOX_USER"
 
 # Cleanup
 $BASE_TOOLS_DIR/scripts/cleanup.sh
