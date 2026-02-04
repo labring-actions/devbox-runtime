6ef4f682feb6f5516bf767484102c9a3c0a18cf1
yy <56745951+lingdie@users.noreply.github.com>
2026-02-04 17:11:13 +0800

    feat(openclaw): fix openclaw, rename to openclaw image name (#95)

--- experimental/runtimes/frameworks/openclaw/latest/project-template/entrypoint.sh
+++ experimental/runtimes/frameworks/openclaw/latest/project-template/entrypoint.sh
@@ -7,4 +7,6 @@ sed -i "s/<OPENAI_BASE_URL>/\${OPENAI_BASE_URL}/g" /home/devbox/.openclaw/opencl
 sed -i "s/<OPENAI_API_KEY>/\${OPENAI_API_KEY}/g" /home/devbox/.openclaw/openclaw.json
 sed -i "s/<OPENAI_MODEL>/\${OPENAI_MODEL}/g" /home/devbox/.openclaw/openclaw.json
 node auto-approve.js &
-exec openclaw gateway --allow-unconfigured --bind lan
+NODE_PID=$!
+trap 'kill "$NODE_PID" 2>/dev/null' EXIT
+openclaw gateway --allow-unconfigured --bind lan
