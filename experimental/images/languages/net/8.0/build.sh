bb12bf591a6c1a75504bb17313e67120cda09891
Alex Lee <3076438032@qq.com>
2026-04-13 18:29:47 +0800

    fix(experimental/net): install ICU dependency

--- experimental/images/languages/net/8.0/build.sh
+++ experimental/images/languages/net/8.0/build.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 arch="$(dpkg --print-architecture)"
 apt-get update && \
-    apt-get install -y ca-certificates curl && \
+    apt-get install -y ca-certificates curl libicu72 && \
     curl -fsSL https://dot.net/v1/dotnet-install.sh -o /tmp/dotnet-install.sh && \
     bash /tmp/dotnet-install.sh --channel 8.0 --install-dir /usr/share/dotnet --architecture "$arch" && \
     ln -sf /usr/share/dotnet/dotnet /usr/bin/dotnet && \
